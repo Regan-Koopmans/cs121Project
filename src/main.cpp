3b60b9a167b414d25680aea141386880f3d8eb7e
ReganKoopmans <regankoopmans@yahoo.com>
2015-09-20 11:11:39 +0200

    Fixed weirdly combined files

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-#include <iostream>
-
-=======
-
 
 #include <iostream>
 
->>>>>>> 5c5300675dff1b2cc352ed5943c9b38cd7371e27
+
+
+#include <iostream>
+
+
 using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
+
   return 0;
 }
-=======
-    return 0;
-}
->>>>>>> 5c5300675dff1b2cc352ed5943c9b38cd7371e27
